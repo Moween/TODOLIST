182e22afa5f0ffec1db36eaa7c58fb6c9b60619d
Moween <moween1998@gmail.com>
2021-03-26 14:59:57 +0100

    Updated codes with the new class names selectors.

--- index.js
+++ index.js
@@ -1,58 +1,43 @@
-const main = document.querySelector('#main');
 document.querySelector('#form').addEventListener('submit', displayTodo);
-
 
 
 function displayTodo(e) {
   e.stopPropagation();
   e.preventDefault();
   //Create a div element to house the div elements
-  const taskWrap = document.createElement('div');
-  
-  taskWrap.className = 'display-todo-list';
-
-  taskWrap.innerHTML = "<h3>Get this done:</h3>";
-
-  //Append div element to the main element
-  main.appendChild(taskWrap);
+  const taskWrap = document.createElement('div');  
+  taskWrap.className = 'task-wrap';
 
 
   //Create p Element
   const taskName = document.createElement('p');
-
   //Add textContent to the p element
-  taskName.textContent = "Task: ";
-  taskName.textContent += document.getElementById('task').value;
-
+  taskName.textContent = "Task: " + document.getElementById('task').value; 
+  taskWrap.appendChild(taskName);
+  
   //Create p Element
   const taskDuration = document.createElement('p');
-
   //Add textContent to the p element
-  taskDuration.textContent = "taskDuration: ";
-  taskDuration.textContent += document.getElementById('taskDuration').value;
-
-  //Append p element to the taskWrap
-  taskWrap.appendChild(taskName);
+  taskDuration.textContent = "Duration: " +  document.getElementById('duration').value;
   taskWrap.appendChild(taskDuration);
-  console.log(taskName);
-  console.log(taskDuration);
 
   //Create deleteBtn
   const deleteBtn = document.createElement('button');
   deleteBtn.setAttribute("type","button");
   deleteBtn.className = ('btn btn-danger btn-sm delete deletebtn');
-  //Add textContent
   deleteBtn.textContent = "X";
-
+  //Add EventListener to delete btn
+  deleteBtn.addEventListener('click', deleteItems);
   //Append btn
   taskWrap.appendChild(deleteBtn);
 
-  document.querySelector('.btn').addEventListener('click', deleteItems);
+  document.querySelector('#todo-list').appendChild(taskWrap);
+
 }
 
 
 
-
 function deleteItems(e) {
-  console.log(1);
+  e.target.parentNode.style.display = 'none';
+  console.log(e.target.parentNode);
 }
