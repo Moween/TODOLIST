b75e839935eaf2a20d38e3491e79fccee9af2c91
Moween <moween1998@gmail.com>
2021-03-26 12:16:27 +0100

    Created a new div element and added comment  in the displayTodo Function.

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
-
+const main = document.querySelector('#main')
 document.querySelector('#form').addEventListener('submit', displayTodo);
 
-const displayBox = document.querySelector('#display-todo-list');
-displayBox.classList.add('invisible');
 
 
 function displayTodo(e) {
   e.stopPropagation();
   e.preventDefault();
-  displayBox.classList.remove('invisible');
+
+  //Create a div element to house the p elements
+  const displayBox = document.createElement('div');
+  displayBox.id = 'display-todo-list';
+
+  displayBox.innerHTML = "<h3>Get this done:</h3>"
+
+  //Append div element to the main element
+  main.appendChild(displayBox)
+
+
+  //Create p Element
   const task = document.createElement('p');
-  task.textContent = document.getElementById('task').value;
+
+  //Add textContent to the p element
+  task.textContent = "Task: "
+  task.textContent += document.getElementById('task').value;
+
+  //Create p Element
   const duration = document.createElement('p');
-  duration.textContent = document.getElementById('duration').value;
+
+  //Add textContent to the p element
+  duration.textContent = "Duration: "
+  duration.textContent += document.getElementById('duration').value;
+
+  //Append p element to the displaybox
   displayBox.appendChild(task);
   displayBox.appendChild(duration);
-  displayBox.appendChild(document.createElement('hr'));
   console.log(task);
   console.log(duration);
 
